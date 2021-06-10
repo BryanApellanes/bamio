5eec7cbb746affaaa2284b9f2da240e38121cc19
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-10 07:50:11 -0700

    update version

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using Bam.Net;
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("ca794a1")]
-[assembly: AssemblySemanticVersion("1.0.5-test+ca794a1")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+ca794a1, Revision=1.0.5.3909649077")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyCommit("9c30314")]
+[assembly: AssemblySemanticVersion("1.0.6-test+9c30314")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-test+9c30314, Revision=1.0.6.1074976821")]
 [assembly: AssemblyProduct("bamdb")]
