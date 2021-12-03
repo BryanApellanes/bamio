5288a51efc95cf24e2726926c8ab2371b5769c7c
Bryan Apellanes <bryan@threeheadz.com>
2021-12-02 19:36:41 -0800

    reorganize

---
+++ bamdb/SemanticAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyCommit("638d95c")]
+[assembly: AssemblySemanticVersion("1.0.6-638d95c")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-638d95c, Revision=1.0.6.813481853")]
+[assembly: AssemblyProduct("bamdb")]
